712d52bea77ac02e446baf0781bf4d188097a3e1
Churjan <churjan@163.com>
2021-12-27 16:53:11 +0800

    todo

--- basic/angular-form/src/app/app.module.ts
+++ basic/angular-form/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormControlComponent } from './form-control/form-control.component';
 import { FormBuilderComponent } from './form-builder/form-builder.component';
+import { DynamicFormItemComponent } from './dynamic-form-item/dynamic-form-item.component';
 
 @NgModule({
-  declarations: [AppComponent, FormControlComponent, FormBuilderComponent],
+  declarations: [AppComponent, FormControlComponent, FormBuilderComponent, DynamicFormItemComponent],
   imports: [BrowserModule, ReactiveFormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
