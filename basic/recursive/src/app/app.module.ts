77d39a814e6ef6765619c6b029edab9b0c1fbe04
Churjan <churjan@163.com>
2022-01-20 08:34:10 +0800

    update

--- basic/recursive/src/app/app.module.ts
+++ basic/recursive/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello/hello.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HelloComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ basic/recursive/src/app/hello/hello.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hello',
+  templateUrl: './hello.component.html',
+  styleUrls: ['./hello.component.scss']
+})
+export class HelloComponent implements OnInit {
+  @Input() count:number=0;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
