3f599a0d1f2190bb37fc2ec9b9969257e434fa51
Churjan <churjan@163.com>
2022-02-08 19:05:11 +0800

    rename

--- basic/use-lodash/karma.conf.js
+++ basic/use-lodash/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true, // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require("path").join(__dirname, "./coverage/angular-lodash"),
+      dir: require("path").join(__dirname, "./coverage/use-lodash"),
       subdir: ".",
       reporters: [{ type: "html" }, { type: "text-summary" }],
     },
--- basic/use-lodash/src/app/app.component.spec.ts
+++ basic/use-lodash/src/app/app.component.spec.ts
@@ -16,10 +16,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-lodash'`, () => {
+  it(`should have as title 'use-lodash'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-lodash');
+    expect(app.title).toEqual('use-lodash');
   });
 
   it('should render title', () => {
@@ -27,7 +27,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'angular-lodash app is running!'
+      'use-lodash app is running!'
     );
   });
 });
