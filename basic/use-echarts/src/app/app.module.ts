fb84c6161fea965fd16767f1ed56312dfcc1fefc
Churjan <churjan@163.com>
2022-02-12 15:04:11 +0800

    update

--- basic/use-echarts/src/app/app.module.ts
+++ basic/use-echarts/src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { NgxEchartsModule } from 'ngx-echarts';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EchartsComponent } from './echarts/echarts.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, EchartsComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgxEchartsModule.forRoot({
+      echarts: () => import('echarts'),
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
