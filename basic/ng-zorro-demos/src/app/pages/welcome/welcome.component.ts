7b0c50d13d116e94690d0514f1795c3a189303f1
Churjan <churjan@163.com>
2022-02-14 17:01:22 +0800

    update

--- basic/ng-zorro-demos/src/app/pages/welcome/welcome.component.ts
+++ basic/ng-zorro-demos/src/app/pages/welcome/welcome.component.ts
@@ -6,6 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./welcome.component.scss'],
 })
 export class WelcomeComponent implements OnInit {
+
+  listOfData= [
+    {
+      key: '1',
+      name: 'John Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park'
+    },
+    {
+      key: '2',
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park'
+    },
+    {
+      key: '3',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park'
+    }
+  ];
+
   constructor() {}
 
   ngOnInit() {}
--- basic/ng-zorro-demos/src/app/pages/welcome/welcome.module.ts
+++ basic/ng-zorro-demos/src/app/pages/welcome/welcome.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 
 import { WelcomeRoutingModule } from './welcome-routing.module';
+import { CommonModule } from '@angular/common';
+import { NgZorroAntdModule } from 'src/app/ng-zorro-antd.module';
 
 import { WelcomeComponent } from './welcome.component';
 
 @NgModule({
-  imports: [WelcomeRoutingModule],
+  imports: [WelcomeRoutingModule, CommonModule, NgZorroAntdModule],
   declarations: [WelcomeComponent],
   exports: [WelcomeComponent],
 })
