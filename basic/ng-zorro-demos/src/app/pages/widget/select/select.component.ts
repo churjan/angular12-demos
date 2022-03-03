f6f3f19d51cfe35c4bee1e97d47cefed86f34945
Churjan <churjan@163.com>
2022-03-03 23:10:26 +0800

    update

--- basic/ng-zorro-demos/src/app/pages/widget/select/select.component.ts
+++ basic/ng-zorro-demos/src/app/pages/widget/select/select.component.ts
@@ -7,36 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SelectComponent implements OnInit {
   selectedKey = 'none';
-  prevSelectedKey='none'
   selectedSort = ''
   sortArr=['','asc','desc']
   timeList = [
     {
-      label: '手动排序',
+      label: '手动',
       value: 'none',
     },
     {
-      label: '按预计开始时间排序',
+      label: '按预计开始时间',
       value: 'ptl_startdate',
     },
     {
-      label: '按预计结束时间排序',
+      label: '按预计结束时间',
       value: 'ptl_enddate',
     },
     {
-      label: '按创建时间排序',
+      label: '按创建时间',
       value: 'createtime',
     },
     {
-      label: '按更新时间排序',
+      label: '按更新时间',
       value: 'updatetime',
     },
     {
-      label: '按实际开始时间排序',
+      label: '按实际开始时间',
       value: 'ptl_realstartdate',
     },
     {
-      label: '按实际结束时间排序',
+      label: '按实际结束时间',
       value: 'ptl_realenddate',
     },
   ];
@@ -46,20 +45,9 @@ export class SelectComponent implements OnInit {
   ngOnInit(): void {}
 
   onChange(e:any){
-    this.prevSelectedKey=e
+    this.selectedSort='asc'
   }
 
-
-  onOptionClick(e:any,data:any){
-    if(e.target.tagName==='SPAN'){
-      if(data.value===this.prevSelectedKey){
-        const idx=this.sortArr.findIndex(item=>item===this.selectedSort)
-        this.selectedSort= this.sortArr[(idx+1)%3]
-      }else{
-        this.selectedSort='asc'
-      }
-    }
-  }
 
   onClick(val:any){
     this.selectedSort=val
