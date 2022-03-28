75d17b3fa1726212134d2a5f6d5449d1184f3c06
Churjan <churjan@163.com>
2022-03-28 21:49:34 +0800

    Update wang-editor.component.ts

--- basic/ng-zorro-demos/src/app/shared/components/wang-editor/wang-editor.component.ts
+++ basic/ng-zorro-demos/src/app/shared/components/wang-editor/wang-editor.component.ts
@@ -23,49 +23,72 @@ import E from 'wangeditor';
   ],
 })
 export class WangEditorComponent
-  implements OnInit,AfterViewInit, OnDestroy, ControlValueAccessor
+  implements OnInit, AfterViewInit, OnDestroy, ControlValueAccessor
 {
   @ViewChild('editorRef') editorRef;
   editor = null;
   constructor() {}
 
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void {}
 
   ngAfterViewInit(): void {
-      this.editor = new E(this.editorRef.nativeElement);
-      // this.editor.config.height = 300;
-      this.editor.config.excludeMenus = ['code', 'video'];
-      this.editor.config.colors = [
-        'rgb(0,0,0)',
-        'rgb(127,127,127)',
-        'rgb(136,0,21)',
-        'rgb(237,28,36)',
-        'rgb(255,127,39)',
-        'rgb(255,242,0)',
-        'rgb(34,177,76)',
-        'rgb(0,162,232)',
-        'rgb(63,72,204)',
-        'rgb(163,73,164)',
-      ];
-      // this.editor.config.customUploadImg = function (resultFiles, insertImgFn) {
-      //   // resultFiles 是 input 中选中的文件列表
-      //   // insertImgFn 是获取图片 url 后，插入到编辑器的方法
+    this.editor = new E(this.editorRef.nativeElement);
+    // this.editor.config.height = 300;
+    this.editor.config.menus = [
+      'head',
+      'bold',
+      'fontSize',
+      'fontName',
+      'italic',
+      'underline',
+      'strikeThrough',
+      'indent',
+      'lineHeight',
+      'foreColor',
+      'backColor',
+      'link',
+      'list',
+      'todo',
+      'justify',
+      // 'quote',
+      'emoticon',
+      'image',
+      // 'video',
+      'table',
+      // 'code',
+      'splitLine',
+      'undo',
+      'redo',
+    ];
+    this.editor.config.colors = [
+      'rgb(0,0,0)',
+      'rgb(127,127,127)',
+      'rgb(136,0,21)',
+      'rgb(237,28,36)',
+      'rgb(255,127,39)',
+      'rgb(255,242,0)',
+      'rgb(34,177,76)',
+      'rgb(0,162,232)',
+      'rgb(63,72,204)',
+      'rgb(163,73,164)',
+    ];
+    // this.editor.config.customUploadImg = function (resultFiles, insertImgFn) {
+    //   // resultFiles 是 input 中选中的文件列表
+    //   // insertImgFn 是获取图片 url 后，插入到编辑器的方法
 
-      //   // 上传图片，返回结果，将图片插入到编辑器中
-      //   insertImgFn(resultFiles);
-      // };
-      this.editor.config.onchange =  (newHtml)=> {
-        this.propagateOnChange(newHtml);
-      };
+    //   // 上传图片，返回结果，将图片插入到编辑器中
+    //   insertImgFn(resultFiles);
+    // };
+    this.editor.config.onchange = (newHtml) => {
+      this.propagateOnChange(newHtml);
+    };
 
-      this.editor.create();
-      // this.editor.txt.html(
-      //   '<h1 style="color:red;">用 JS 设置的内容</h1><h2>用 JS 设置的内容</h2>'
-      // );
-      // console.log(this.editor.txt.html());
-      // console.log(this.editor.txt.text());
+    this.editor.create();
+    // this.editor.txt.html(
+    //   '<h1 style="color:red;">用 JS 设置的内容</h1><h2>用 JS 设置的内容</h2>'
+    // );
+    // console.log(this.editor.txt.html());
+    // console.log(this.editor.txt.text());
   }
 
   ngOnDestroy(): void {
@@ -74,11 +97,11 @@ export class WangEditorComponent
   }
 
   writeValue(value) {
-    console.log(111,value)
+    console.log(111, value);
     if (value) {
-      setTimeout(()=>{
-        this.editor.txt.html(value)
-      },300)
+      setTimeout(() => {
+        this.editor.txt.html(value);
+      }, 300);
     }
   }
   propagateOnChange: (value: any) => void = (_: any) => {};
