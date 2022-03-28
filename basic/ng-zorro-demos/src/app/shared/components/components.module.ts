ed4c7c5bbe18571113a7d1732182e5a3e4045d93
Churjan <churjan@163.com>
2022-03-28 08:55:28 +0800

    优化代码

---
+++ basic/ng-zorro-demos/src/app/shared/components/components.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from '@angular/core';
+import { WangEditorModule } from './wang-editor/wang-editor.module';
+
+@NgModule({
+  exports: [WangEditorModule],
+})
+export class ComponentsModule {}
--- basic/ng-zorro-demos/src/app/shared/shared.module.ts
+++ basic/ng-zorro-demos/src/app/shared/shared.module.ts
@@ -1,14 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ModulesModule } from './modules/modules.module';
-import { WangEditorModule } from './wang-editor/wang-editor.module';
+import { ComponentsModule } from './components/components.module';
 @NgModule({
-  imports: [
-    CommonModule
-  ],
   exports:[
     ModulesModule,
-    WangEditorModule
+    ComponentsModule,
   ]
 })
 export class SharedModule { }
