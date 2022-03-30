655487f5c4209fc92864f38d29aba765ab565d5f
Churjan <churjan@163.com>
2022-03-30 21:55:25 +0800

    删除没用的代码

--- basic/ng-zorro-demos/src/app/shared/components/components.module.ts
+++ basic/ng-zorro-demos/src/app/shared/components/components.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
-import { WangEditorModule } from './wang-editor/wang-editor.module';
 import { WangEditor5Module } from './wang-editor5/wang-editor5.module';
-import { NgxQuillEditorModule } from './ngx-quill-editor/ngx-quill-editor.module';
 @NgModule({
-  exports: [WangEditorModule, WangEditor5Module, NgxQuillEditorModule],
+  exports: [WangEditor5Module],
 })
 export class ComponentsModule {}
