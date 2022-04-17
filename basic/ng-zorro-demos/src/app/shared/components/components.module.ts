3d8f3d54b9deb48694e22496cf3102ebab38c3f8
Churjan <churjan@163.com>
2022-04-17 17:47:52 +0800

    update

--- basic/ng-zorro-demos/src/app/shared/components/components.module.ts
+++ basic/ng-zorro-demos/src/app/shared/components/components.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { WangEditor5Module } from './wang-editor5/wang-editor5.module';
+import { CategoryModule } from './category/category.module';
 @NgModule({
-  exports: [WangEditor5Module],
+  exports: [WangEditor5Module, CategoryModule],
 })
 export class ComponentsModule {}
