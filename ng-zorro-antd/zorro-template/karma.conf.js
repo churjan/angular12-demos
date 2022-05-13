368c292c6771f951daff4eaa7727178de650069e
Churjan <churjan@163.com>
2022-05-13 08:56:32 +0800

    update

--- ng-zorro-antd/zorro-template/karma.conf.js
+++ ng-zorro-antd/zorro-template/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/zorro-import-icon'),
+      dir: require('path').join(__dirname, './coverage/zorro-template'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- ng-zorro-antd/zorro-template/src/app/app.component.spec.ts
+++ ng-zorro-antd/zorro-template/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'zorro-import-icon'`, () => {
+  it(`should have as title 'zorro-template'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('zorro-import-icon');
+    expect(app.title).toEqual('zorro-template');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('zorro-import-icon app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('zorro-template app is running!');
   });
 });
