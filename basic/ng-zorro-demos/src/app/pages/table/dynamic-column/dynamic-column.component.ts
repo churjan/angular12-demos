d9b949a75979e2dcac874770fe82dcc0b99beb78
Churjan <churjan@163.com>
2022-08-10 15:09:07 +0800

    update

--- basic/ng-zorro-demos/src/app/pages/table/dynamic-column/dynamic-column.component.ts
+++ basic/ng-zorro-demos/src/app/pages/table/dynamic-column/dynamic-column.component.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NzResizableModule } from 'ng-zorro-antd/resizable';
+import { NzResizeEvent } from 'ng-zorro-antd/resizable';
 @Component({
   selector: 'app-dynamic-column',
   templateUrl: './dynamic-column.component.html',
-  styleUrls: ['./dynamic-column.component.scss']
+  styleUrls: ['./dynamic-column.component.less'],
 })
 export class DynamicColumnComponent implements OnInit {
+  cols = [
+    {
+      title: 'Name',
+      width: '180px',
+    },
+    {
+      title: 'Age',
+      width: '180px',
+    },
+    {
+      title: 'Address',
+      width: '200px',
+    },
+    {
+      title: 'Actions',
+    },
+  ];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  listOfData = [
+    {
+      key: '1',
+      name: 'John Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park',
+    },
+    {
+      key: '2',
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park',
+    },
+    {
+      key: '3',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park',
+    },
+  ];
+  onResize({ width }: NzResizeEvent, col: string): void {
+    this.cols = this.cols.map((e) =>
+      e.title === col ? { ...e, width: `${width}px` } : e
+    );
   }
-
+  ngOnInit(): void {}
 }
--- basic/ng-zorro-demos/src/app/pages/table/table.module.ts
+++ basic/ng-zorro-demos/src/app/pages/table/table.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 import { TableRoutingModule } from './table-routing.module';
 import { DynamicColumnComponent } from './dynamic-column/dynamic-column.component';
@@ -13,7 +14,8 @@ import { TableComponent } from './table/table.component';
   ],
   imports: [
     CommonModule,
-    TableRoutingModule
+    TableRoutingModule,
+    SharedModule,
   ]
 })
 export class TableModule { }
